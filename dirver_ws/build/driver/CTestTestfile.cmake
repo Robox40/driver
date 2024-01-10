55efba5f9c2b143628cbc25423595634b6310546
Robox40 <ichraibi58@gmail.com>
2024-01-10 16:16:54 +0100

    clean repo

--- dirver_ws/build/driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ilyas/workspace/dirver_ws/src/driver
-# Build directory: /home/ilyas/workspace/dirver_ws/build/driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
